dc29b9089f2e6ed530d0f55ed1a6e78b1a19934a
astillo <clomes.alex@gmail.com>
2019-06-11 15:40:34 -0500

    adding files

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,43 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(element => fullName.push(element.first_name +" "+ element.last_name))
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map(element => { let noCaps = element.first_name; allCaps.push(noCaps.toUpperCase())})
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+runners.filter(element => {if(element.shirt_size == "L"){largeShirts.push(element)}})
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+runners.forEach(element => {ticketPriceTotal.push(element.donation);})
+ticketPriceTotal = ticketPriceTotal.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+runnerEmails = [];
+runners.forEach(element => {runnerEmails.push(element.email)})
+console.log(runnerEmails)
 // Problem 2
-
+runnerEmailDonation = [];
+runners.forEach(element => {runnerEmailDonation.push(element.email, element.donation)})
+console.log(runnerEmailDonation)
 // Problem 3
+let highestDonation = [];
+runners.forEach(element => {highestDonation.push(element.donation)})
+highestDonation.sort((a,b)=>a-b)
+console.log("The Highest Donation was $", highestDonation.slice(-1)[0])
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,24 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const string = () => {
+  let name = "Alex"
+  displayName = () =>{
+    alert(name)
+  }
+  displayName() 
+}
+string()
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+let add = (function () {
+  let counter = 0;
+  return function () { counter += 1; return counter }
+})();
+console.log(add())
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
